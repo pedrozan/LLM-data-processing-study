dc13e07ebb301edb5f25f29e3a0e06d7990785fd
Pedro Henrique <pedro.schleder@gmail.com>
2026-01-09 00:47:38 +0000

    feat: normalize timestamp fields in stg_support_tickets model

--- dbt_project/models/staging/stg_support_tickets.sql
+++ dbt_project/models/staging/stg_support_tickets.sql
@@ -21,8 +21,15 @@ select
     resolution,
     ticket_priority,
     ticket_channel,
-    first_response_time,
-    time_to_resolution,
+    -- Normalize timestamps: convert to TIMESTAMP type, preserve nulls
+    case
+        when first_response_time is null or first_response_time = '' then null
+        else try_cast(first_response_time as timestamp)
+    end as first_response_time,
+    case
+        when time_to_resolution is null or time_to_resolution = '' then null
+        else try_cast(time_to_resolution as timestamp)
+    end as time_to_resolution,
     customer_satisfaction_rating,
     current_timestamp as loaded_at
 
